86372dc6c33945ff062bb14ec27b1b1e1fa37e29
appleschaussaa <robertschauss91@gmail.com>
2024-04-19 15:58:53 -0700

    Update vite.config.js with new dependencies and base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Obsidian-Beerhall/",
+  optimizeDeps: {
+      include: [
+          "@mui/material/Tooltip",
+          "@emotion/styled",
+          "@mui/icons-material",
+          "@mui/material/Unstable_Grid2",
+      ],
+  },
 })
