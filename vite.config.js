91b71f029618d0b3360d0d604ee20f488a267644
appleschaussaa <robertschauss91@gmail.com>
2024-04-21 16:31:30 -0700

    Fix broken links and enable history API fallback in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
   base: "/deployed-ObsidianBH/",
   optimizeDeps: {
       include: [
